0e42ae1ff212d259f900fdcd34f3159a6c7f7a11
Yupu-Chen <godferychan12@gmail.com>
2023-09-19 21:55:34 +0400

    photo carousel added

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const about = document.getElementById("about");
 const homeBtn = document.getElementById("home-btn");
 const blogBtn = document.getElementById("blog-btn");
 const aboutBtn = document.getElementById("about-btn");
+const slides = document.querySelectorAll(".slide");
 
 const MFF30 = document.getElementById("MFF30");
 
-MFF30.onclick = function(){
+MFF30.onclick = function () {
     if (window.getComputedStyle(blogs, null).getPropertyValue("visibility") == "hidden") {
         blogs.style.visibility = "visible";
         home.style.visibility = "hidden";
@@ -53,3 +54,45 @@ function makeVisible() {
         }
     }
 }
+
+slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// current slide counter
+let curSlide = 0;
+
+let maxSlide = slides.length - 1;
+
+// select next slide button
+const nextSlide = document.querySelector("#next");
+
+// add event listener and next slide functionality
+nextSlide.addEventListener("click", function () {
+    if (curSlide == maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
+
+// select prev slide button
+const prevSlide = document.querySelector("#prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  //   move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
